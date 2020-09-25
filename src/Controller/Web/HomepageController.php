03a72a6504e41a3af83095a1deaeb372cf9dbddb
Glenn Rademakers <glennrademakers@hotmail.com>
2020-09-25 15:56:00 +0200

    2020-09-25 Restructure Vegas - Homepage planned

--- src/Controller/Web/HomepageController.php
+++ src/Controller/Web/HomepageController.php
@@ -6,20 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Service\VacancyService;
+use App\Service\PlatformService;
 
 class HomepageController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(VacancyService $vs)
+    public function index(VacancyService $vs, PlatformService $ps)
     {
         $vacancies = $vs->getRecentVacancies();
-
+        $platforms = $ps->getAllPlatforms();
+        
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'Homepage Controller', 
             'vacancies'=>$vacancies,
-        ]);
+            'platforms'=>$platforms,]);
     }
 
      /**
@@ -31,7 +33,6 @@ class HomepageController extends AbstractController
 
         return $this->render('homepage/alternative/index.html.twig', [
             'controller_name' => 'Alternative Homepage Controller', 
-            'vacancies'=>$vacancies,
-        ]);
+            'vacancies'=>$vacancies,]);
     }
 }
